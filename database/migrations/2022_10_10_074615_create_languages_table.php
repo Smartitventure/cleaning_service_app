b2483188adca1c64b7335d9b825c800ac7300d64
AviJanagal <avinashsmartitventures@gmail.com>
2022-10-10 13:20:51 +0530

    chnages

---
+++ database/migrations/2022_10_10_074615_create_languages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+            $table->char('image');
+            $table->string('name');
+            $table->dateTime('createdAt')->nullable();
+            $table->dateTime('updatedAt')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('languages');
+    }
+}
